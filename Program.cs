34b2b403450b1a33b526895b9d58ada461a2987f
dexmo007 <henrik.drefs@outlook.de>
2016-03-04 10:33:30 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace hack
+{
+    internal static class Program
+    {
+
+        private const string HostsPath = @"C:\Windows\System32\drivers\etc\hosts";
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main()
+        {
+            using (var sw = new StreamWriter(new FileStream(HostsPath, FileMode.Append)))
+            {
+                sw.WriteLine();
+                sw.WriteLine("127.0.0.1 www.facebook.com");
+                sw.WriteLine("127.0.0.1 facebook.com");
+            }
+        }
+    }
+}
